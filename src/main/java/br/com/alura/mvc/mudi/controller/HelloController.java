82eafb53ed3ab146bbe9a671ae171901be7434f1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-18 11:18:22 -0300

    Conexão do Java com o HTML criado

--- src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -1,9 +1,14 @@
 package br.com.alura.mvc.mudi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
 
 	//Isso é um action, que irá processar a requisição do usuário e redirecionar para uma view.
+	@GetMapping("/hello")
 	public String hello() {
-		return "hellow";
+		return "hello";
 	}
 }
