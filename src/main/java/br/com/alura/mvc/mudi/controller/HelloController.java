b0888fe3511c86ff24b7b09e5c90f3ae4f075605
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-18 11:04:40 -0300

    Criação da primeira action

--- src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -2,4 +2,8 @@ package br.com.alura.mvc.mudi.controller;
 
 public class HelloController {
 
+	//Isso é um action, que irá processar a requisição do usuário e redirecionar para uma view.
+	public String hello() {
+		return "hellow";
+	}
 }
