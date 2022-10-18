9c6a5ced9491af9d132bb78be22a14c90ae6c96e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-18 14:23:57 -0300

    Troca de HtppServlet para model

--- src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -1,6 +1,9 @@
 package br.com.alura.mvc.mudi.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +11,8 @@ public class HelloController {
 
 	//Isso é um action, que irá processar a requisição do usuário e redirecionar para uma view.
 	@GetMapping("/hello")
-	public String hello() {
+	public String hello(Model model) {
+		model.addAttribute("nome", "Mundo"); //settando o valor do atributo
 		return "hello";
 	}
 }
