31b6cf25bf159411cfe0839f470176eefe7fa917
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-19 08:26:57 -0300

    Adição do bootstrap no projeto

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,6 +1,10 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.alura.mvc.mudi.model.Pedido;
@@ -9,12 +13,17 @@ import br.com.alura.mvc.mudi.model.Pedido;
 public class HomeController {
 
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		Pedido pedido = new Pedido();
 		pedido.setNomeProduto("Xiaomi Redmi Note 8");
 		pedido.setUrlImagem("https://m.media-amazon.com/images/I/714OLO-uw1L._AC_SL1500_.jpg");
 		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Redmi-Global-Mineral/dp/B07YB7NLY7");
 		pedido.setDescricao("Uma descrição qualquer para este pedido.");
+		
+		//para criar uma lista de pedidos
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		model.addAttribute("pedidos", pedido);
+		
 		return "home";
 	}
 }
