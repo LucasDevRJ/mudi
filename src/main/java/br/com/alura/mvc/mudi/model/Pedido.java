bb99165646111c6a7448a88b34bdbfc0626e8d06
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-19 07:37:54 -0300

    Adição dos atributos da classe Produto

--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -1,5 +1,14 @@
 package br.com.alura.mvc.mudi.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Pedido {
 
+	private String nomeProduto;
+	private BigDecimal valorNegociado;
+	private LocalDate dataDaEntrega;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
 }
