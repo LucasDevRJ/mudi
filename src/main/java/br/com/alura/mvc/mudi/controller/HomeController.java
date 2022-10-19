f377a740c6c9914d53a5723fbb30afdf61d7bd6e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-19 13:39:05 -0300

    Criação da conexão com o banco de dados usando o Entity Manager

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,9 +1,13 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,19 +15,27 @@ import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Xiaomi Redmi Note 8");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/714OLO-uw1L._AC_SL1500_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Redmi-Global-Mineral/dp/B07YB7NLY7");
-		pedido.setDescricao("Uma descrição qualquer para este pedido.");
 		
-		//para criar uma lista de pedidos
-		List<Pedido> pedidos = Arrays.asList(pedido);
-		model.addAttribute("pedidos", pedido);
+		Query query = entityManager.createQuery("SELECT p FROM Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
+		
+		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
+	}
+	
+	public static void main(String[] args) {
+		
+		Model model = new ExtendedModelMap();
+		
+		HomeController homeController = new HomeController();
+		homeController.home(model);
+		
 	}
 }
