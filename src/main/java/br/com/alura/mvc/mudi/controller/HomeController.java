e90a54511c3b3055630c90132b22939bf9a5b89f
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-19 07:45:43 -0300

    Settagem dos atributos do produto

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,11 +3,18 @@ package br.com.alura.mvc.mudi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.alura.mvc.mudi.model.Pedido;
+
 @Controller
 public class HomeController {
 
 	@GetMapping("/home")
 	public String home() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("Xiaomi Redmi Note 8");
+		pedido.setUrlImagem("https://m.media-amazon.com/images/I/714OLO-uw1L._AC_SL1500_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Redmi-Global-Mineral/dp/B07YB7NLY7");
+		pedido.setDescricao("Uma descrição qualquer para este pedido.");
 		return "home";
 	}
 }
