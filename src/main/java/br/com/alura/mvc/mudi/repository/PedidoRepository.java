a6fb96cd3764eda853297486747f2314bdf58a49
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-20 13:07:07 -0300

    Remoção de método redundante na interface

--- src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/alura/mvc/mudi/repository/PedidoRepository.java
@@ -1,7 +1,5 @@
 package br.com.alura.mvc.mudi.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +8,4 @@ import br.com.alura.mvc.mudi.model.Pedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 	
-	public List<Pedido> findAll();
 }
