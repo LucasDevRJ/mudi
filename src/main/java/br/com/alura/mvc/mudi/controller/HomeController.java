de6d4ac3d8e34c4584e26a8567a67286f1dacf67
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-20 13:08:06 -0300

    Chamando o método findAll

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
 	@GetMapping("/home")
 	public String home(Model model) {
 		
-		List<Pedido> pedidos = pedidoRepository.recuperaTodosPedidos();
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", pedidos);
 		
 		return "home";
