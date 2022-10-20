bd4526f902ad94b9e5fab4d455c146bb76187cd0
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-20 15:36:38 -0300

    Setter dos valores dos pedidos cadastrados

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.alura.mvc.mudi.dto.RequisicaoNovoPedido;
+import br.com.alura.mvc.mudi.model.Pedido;
+
 @Controller
 @RequestMapping("pedido") //todas as requisições de pedido virão para cá
 public class PedidoController {
@@ -16,6 +19,9 @@ public class PedidoController {
 	
 	@PostMapping("novo")
 	public String novo(RequisicaoNovoPedido requisicao) {
+		
+		Pedido pedido = requisicao.toPedido();
+		
 		return "pedido/formulario";
 	}
 }
