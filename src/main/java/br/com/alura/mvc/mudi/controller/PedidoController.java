b5d1d1204d095b5e93422d2b58b020861db3528d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-10-20 14:48:02 -0300

    Redirecionamento para a página de formulário

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -1,8 +1,15 @@
 package br.com.alura.mvc.mudi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("pedido") //todas as requisições de pedido virão para cá
 public class PedidoController {
 
+	@GetMapping("formulario")
+	public String formulario() {
+		return "pedido/formulario";
+	}
 }
