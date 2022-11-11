52f5af219f8d586dc37c7228f9cbd5b95f12714c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-11 11:46:46 -0300

    Criação dos métodos Getters e Setters.

--- src/main/java/br/com/alura/mvc/mudi/model/User.java
+++ src/main/java/br/com/alura/mvc/mudi/model/User.java
@@ -10,4 +10,28 @@ public class User {
 	private String username;
 	private String password;
 	private Boolean enabled;
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Boolean getEnabled() {
+		return enabled;
+	}
+	
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
 }
