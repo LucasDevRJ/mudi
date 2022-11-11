1c4b5058ed3838f77e16d5a5d3a0e2b7a9000571
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-11 11:45:29 -0300

    Criação dos atributos e junção com a tabela users.

--- src/main/java/br/com/alura/mvc/mudi/model/User.java
+++ src/main/java/br/com/alura/mvc/mudi/model/User.java
@@ -1,5 +1,13 @@
 package br.com.alura.mvc.mudi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class User {
 
+	private String username;
+	private String password;
+	private Boolean enabled;
 }
