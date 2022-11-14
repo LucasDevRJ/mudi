5dec9eb8b08b4c7af55c248747126b24b0613985
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-14 20:41:20 -0300

    Criação dos Getters e Setters.

--- src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovaOferta.java
@@ -3,4 +3,42 @@ package br.com.alura.mvc.mudi.dto;
 public class RequisicaoNovaOferta {
 
 	private Long pedidoId;
+	
+	private String valor;
+	
+	private String dataDaEntrega;
+	
+	private String comentario;
+
+	public Long getPedidoId() {
+		return pedidoId;
+	}
+
+	public void setPedidoId(Long pedidoId) {
+		this.pedidoId = pedidoId;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public String getDataDaEntrega() {
+		return dataDaEntrega;
+	}
+
+	public void setDataDaEntrega(String dataDaEntrega) {
+		this.dataDaEntrega = dataDaEntrega;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
 }
