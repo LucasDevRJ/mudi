7189b812d32b17ab9ff263dbaa160e0a7ea4bf01
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-16 17:34:02 -0300

    Classe interceptora finalizada.

---
+++ src/main/java/br/com/alura/mvc/mudi/WebConfig.java
@@ -0,0 +1,7 @@
+package br.com.alura.mvc.mudi;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+public class WebConfig extends WebMvcConfigurationSupport{
+
+}
