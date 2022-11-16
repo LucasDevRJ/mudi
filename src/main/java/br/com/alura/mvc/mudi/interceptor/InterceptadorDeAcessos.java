97a81515523f4fabeb159f43868260b49c996ec1
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-16 17:20:41 -0300

    Criação do interceptor de acessos.

---
+++ src/main/java/br/com/alura/mvc/mudi/interceptor/InterceptadorDeAcessos.java
@@ -0,0 +1,7 @@
+package br.com.alura.mvc.mudi.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class InterceptadorDeAcessos extends HandlerInterceptorAdapter{
+
+}
